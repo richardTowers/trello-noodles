4e39801c1eeac1bd6c641b4defe2250820447a10
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2020-07-07 14:34:53 +0100

    Use html node labels to show tags

--- digraph.rb
+++ digraph.rb
@@ -10,7 +10,7 @@ end
 TRELLO_BOARD_ID = 'QiyLAAxa'
 CARD_PATTERN = /https:\/\/trello[.]com\/c\/[A-Za-z0-9]+/
 
-GraphNode = Struct.new(:id, :label, :href, :desc)
+GraphNode = Struct.new(:id, :label, :href, :desc, :tags)
 GraphEdge = Struct.new(:from, :to)
 
 def get_card_url_prefix(url)
@@ -27,6 +27,7 @@ def main
       card.name.gsub('"', '\"'),
       card.url,
       card.desc,
+      card.labels.map{ |l| l.name },
     )
   end
 
@@ -47,11 +48,16 @@ def main
 
     <% nodes_with_edges.each do |node| %>
     "<%= node.id %>" [
-      label = "<%= node.label %>"
-      href = "<%= node.href %>"
-      shape = "box"
+      label = <
+        <table border="0" cellborder="1" cellspacing="0" cellpadding="4">
+          <tr><td href="<%= node.href %>"><font color="#1d70b8"><u><%= node.label %></u></font></td></tr>
+          <% node.tags.each do |tag| %>
+          <tr><td><%= tag %></td></tr>
+          <% end %>
+        </table>
+      >
+      shape = none
       fontname = "Arial"
-      fontcolor = "#1d70b8"
     ]
     <% end %>
 
